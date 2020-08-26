6a228a9324d56d121b92034e710d9ea55464b118
Yufan You <ouuansteve@gmail.com>
2020-08-26 18:48:07 +0800

    feat(kabu): no "要来买点吗？" when can't afford & service fee (#11)
    * fix(kabu): no "要来买点吗？" when can't afford
    
    * feat(kabu): add service fee
    
    * feat(kabu): add hint of checkin

--- src/plugins/kabu.ts
+++ src/plugins/kabu.ts
@@ -20,10 +20,12 @@ interface Stock {
 
 interface Config {
     expireDays: number,
+    serviceFee: number,
 }
 
 const defaultConfig = {
     expireDays: 7,
+    serviceFee: 0.03,
 };
 
 export const apply = (app: App, config: Config) => {
@@ -63,16 +65,16 @@ export const apply = (app: App, config: Config) => {
                     sum += number;
                     stockList += `你有 ${number} 棵以 ${buyPrice} 个硬币每棵买入的大头菜，它们会在 ${moment(expire).fromNow()} 烂掉。\n`;
                 }
-                if (count === 0) stockList = '你现在手上还没有大头菜，要来买点吗？';
-                else if (count > res.length) stockList += `隐藏了 ${count - res.length} 个条目。`;
-                if (!session.$user.coin) session.$user.coin = 0;
                 const price = await priceToday(session);
+                if (!session.$user.coin) session.$user.coin = 0;
+                if (count === 0) stockList = `你现在手上还没有大头菜${session.$user.coin >= price ? '，要来买点吗？' : '。你可以使用 checkin 来获取硬币。'}`;
+                else if (count > res.length) stockList += `隐藏了 ${count - res.length} 个条目。`;
                 return `你现在共有 ${sum} 棵大头菜和 ${session.$user.coin} 个硬币。
 今天卖给 ${session.$username} 的大头菜价格是每棵 ${price} 硬币。
 ${stockList}`;
             });
 
-        app.command('kabu.buy [number]', '购买大头菜。若不指定数量则尽量多地购买。')
+        app.command('kabu.buy [number]', `购买大头菜。若不指定数量则尽量多地购买。手续费 ${config.serviceFee * 100}%。`)
             .shortcut('购买大头菜', { prefix: false, fuzzy: true })
             .userFields(['coin'])
             .action(async ({ session }, arg) => {
@@ -92,12 +94,13 @@ ${stockList}`;
                     buyPrice: price,
                     expire: expire.toDate(),
                 });
-                session.$user.coin -= price * number;
-                return `你花了 ${price * number} 个硬币以 ${price} 每棵的价格购买了 ${number} 棵大头菜。
+                const cost = Math.ceil((1 + config.serviceFee) * price * number);
+                session.$user.coin -= cost;
+                return `你花了 ${cost} 个硬币（含 ${cost - price * number} 个硬币的手续费）以 ${price} 每棵的价格购买了 ${number} 棵大头菜。
 要是你没有在 ${config.expireDays} 天内把大头菜卖掉，它们就会全部烂掉，害你大亏本！一定要注意这一点喔。`;
             });
 
-        app.command('kabu.sell [number]', '卖出最早购买（最先烂掉）的大头菜。若不指定数量则全部卖出。')
+        app.command('kabu.sell [number]', `卖出最早购买（最先烂掉）的大头菜。若不指定数量则全部卖出。手续费 ${config.serviceFee * 100}%。`)
             .shortcut('卖出大头菜', { prefix: false, fuzzy: true })
             .userFields(['coin'])
             .action(async ({ session }, arg) => {
@@ -120,11 +123,11 @@ ${stockList}`;
                 if (sum === 0 || (sellNumber !== Infinity && sum !== sellNumber)) return '你没有足够多的大头菜来卖出！';
                 const price = await priceToday(session);
                 if (!session.$user.coin) session.$user.coin = 0;
-                const gain = sum * price;
+                const gain = Math.floor((1 - config.serviceFee) * sum * price);
                 session.$user.coin += gain;
                 if (deleteIds.length) await stockColl.deleteMany({ _id: { $in: deleteIds } });
                 if (update) await stockColl.updateOne({ _id: update._id }, { $set: { number: update.newNumber } });
-                return `你已成功卖出 ${sum} 棵大头菜，获得了 ${gain} 个硬币！`;
+                return `你已成功卖出 ${sum} 棵大头菜，获得了 ${gain} 个硬币（已扣除 ${sum * price - gain} 个硬币的手续费）！`;
             });
     });
 
