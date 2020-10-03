ca4fbda5e6106d244680e968c1a929a1b8d5d466
undefined <masnn0@outlook.com>
2020-10-03 16:09:28 +0800

    Update server.py

--- lib/server.py
+++ lib/server.py
@@ -20,11 +20,11 @@ tensorStr = lambda a: str(a)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        ipt = json.loads(body)
-        print(ipt)
         try:
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            ipt = json.loads(body)
+            print(ipt)
             image = Image.open(ipt['path'])
             input_tensor = preprocess(image)
             input_batch = input_tensor.unsqueeze(0)
@@ -37,13 +37,13 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             tags = list()
             for i in inds[0: len(tmp)]:
                 tags.append([int(re.sub("\D", "", str(i))), float(probs[i].detach().numpy())])
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(str.encode(json.dumps(tags)))
         except Exception as e:
             self.send_response(200)
             self.end_headers()
             self.wfile.write(str.encode(str(e)))
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(str.encode(json.dumps(tags)))
 
 httpd = HTTPServer(('127.0.0.1', 10377), SimpleHTTPRequestHandler)
 httpd.serve_forever()
