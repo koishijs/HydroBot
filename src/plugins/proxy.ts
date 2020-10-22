e44238eee2b9887a5016f007fdd2fa1a8b60a459
undefined <masnn0@outlook.com>
2020-10-22 12:50:42 +0800

    better proxy

--- src/plugins/proxy.ts
+++ src/plugins/proxy.ts
@@ -3,13 +3,35 @@ import { getTargetId, Session } from 'koishi-core/dist/session';
 
 export const apply = (ctx: Context) => {
     ctx.command('proxy <command...>', 'Proxy a command', { hidden: true, authority: 4 })
-        .option('id', '-u <userId>')
+        .option('user', '-u <userId>')
+        .option('self', '-s <selfId>')
         .option('group', '-g <groupId>')
         .option('length', '-l <length>', { fallback: 1 })
         .action(async ({ session, options }, command) => {
-            const newSession = new Session(session.$app, session);
-            newSession.userId = getTargetId(options.id);
-            newSession.groupId = getTargetId(options.group);
+            const newSession = new Session(ctx.app, session);
+            delete newSession.groupId;
+            if (options.group) {
+                newSession.groupId = +options.group;
+                newSession.messageType = 'group';
+                newSession.subType = 'normal';
+            } else {
+                newSession.messageType = 'private';
+                newSession.subType = 'other';
+            }
+            if (options.self) newSession.selfId = +options.self;
+            if (options.user) {
+                const id = getTargetId(options.user);
+                if (!id) return '未指定目标。';
+                newSession.userId = id;
+                newSession.sender.userId = id;
+            }
+            if (options.group) {
+                const info = await session.$bot.getGroupMemberInfo(newSession.groupId, newSession.userId).catch(() => ({}));
+                Object.assign(newSession.sender, info);
+            } else if (options.user) {
+                const info = await session.$bot.getStrangerInfo(newSession.userId).catch(() => ({}));
+                Object.assign(newSession.sender, info);
+            }
             await newSession.$send(command);
             let message = '';
             // eslint-disable-next-line no-await-in-loop
