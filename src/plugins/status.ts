ed2836a1442ea5af80df082667aa3a0b5bdd909b
undefined <masnn0@outlook.com>
2021-01-01 13:50:09 +0800

    update status

--- src/plugins/status.ts
+++ src/plugins/status.ts
@@ -1,5 +1,6 @@
+/* eslint-disable no-template-curly-in-string */
 import { totalmem, freemem } from 'os';
-import { BotStatusCode, Context } from 'koishi-core';
+import { Context } from 'koishi-core';
 import { apply as KoishiPluginStatus, extendStatus } from 'koishi-plugin-status';
 import moment from 'moment';
 
@@ -7,30 +8,23 @@ declare module 'koishi-plugin-status' {
     interface Status {
         totalSendCount: number,
         totalReceiveCount: number,
+        freemem: number,
+        usedmem: number,
     }
 }
 
 export async function apply(ctx: Context) {
     ctx.plugin(KoishiPluginStatus, {
-        output({
-            bots, cpu, activeUsers, activeGroups, totalSendCount, totalReceiveCount,
-        }) {
-            const output = bots
-                .filter((bot) => bot.code !== BotStatusCode.BOT_IDLE)
-                .map(({
-                    label, selfId, code, rate,
-                }) => `${label || selfId}：${code ? '无法连接' : `工作中（${rate}/min）`}`);
-
-            output.push('==========');
-            output.push(
-                `活跃用户数量：${activeUsers}`,
-                `活跃群数量：${activeGroups}`,
-                `CPU 使用率：${(cpu.total * 100).toFixed()}%`,
-                `内存使用量：${Math.floor((totalmem() - freemem()) / 1024 / 1024)}M / ${Math.floor(totalmem() / 1024 / 1024)}M`,
-                `今日收发消息量 ${totalReceiveCount}/${totalSendCount}`,
-            );
-            return output.join('\n');
-        },
+        formatBot: '{{ label || selfId }}：{{ code ? `无法连接` : `工作中（${rate}/min）` }}',
+        format: [
+            '{{ bots }}',
+            '==========',
+            '活跃用户数量：{{ activeUsers }}',
+            '活跃群数量：{{ activeGroups }}',
+            'CPU 使用率：{{ (cpu.total * 100).toFixed() }%',
+            '内存使用量：{{ usedmem }}M / {{ totalmem }}M',
+            '今日收发消息量 {{ totalReceiveCount }}/{{ totalSendCount }}',
+        ].join('\n'),
     });
 
     ctx.app.on('connect', () => {
@@ -41,6 +35,8 @@ export async function apply(ctx: Context) {
             const time = { time: { $gt: moment().add(-1, 'day').toDate() } };
             status.totalSendCount = await c.find({ ...time, sender: { $in: bots } }).count();
             status.totalReceiveCount = await c.find({ ...time, sender: { $nin: bots } }).count();
+            status.usedmem = Math.floor((totalmem() - freemem()) / 1024 / 1024);
+            status.freemem = Math.floor(freemem() / 1024 / 1024);
         }, true);
     });
 }
